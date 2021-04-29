8f6e0729483f95053e0e4c643f0517b8a4a8ea7c
Weiqifan1 <cmlykke@hotmail.com>
2021-04-29 17:56:35 +0200

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+object Main {
+
+  def computeNumber(): Int = 5+2
+
+  def main(args: Array[String]): Unit = {
+    println("hej lykke")
+
+    val number = computeNumber()
+
+    //val lines: String = scala.io.Source.fromFile("src/main/resources/hanzifiles/zz201906_test.txt").mkString
+
+    //val eachline: Array[String] = lines.split("\n")
+
+
+    println("farvel lykke ")// + eachline.length)
+  }
+}
